31dc3744ba6dbd128f9925e22c26e8b5fc6545fc
Abhijeet <abhijeetpatil2590@gmail.com>
2023-08-29 09:56:28 +0530

    Fibonacci series using for loop

---
+++ src/training/Fibonacciseries.java
@@ -0,0 +1,20 @@
+package training;
+
+public class Fibonacciseries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int n1=0, n2=1,n3,i,count=10;
+		 System.out.println("Fibonacci Series till " + count + " terms:");
+		 System.out.print(n1);
+		 for(i=1;i<count; ++i)
+		 {
+			 n3=n1+n2;
+			 System.out.print(","+n3);
+			 n1=n2;
+			 n2=n3;
+		 }
+	}
+
+}
