24bab3c7ba9d4aa0111fab62ce2d2b7d9b563beb
Abhijeet <abhijeetpatil2590@gmail.com>
2023-09-04 10:42:58 +0530

    Read character,store in string and Maximum Character in a String

--- src/training/Filereaderandcharcount.java
+++ src/training/Filereaderandcharcount.java
@@ -7,9 +7,30 @@ public class Filereaderandcharcount {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
- File file=new File("C:\\Selenium training\\Fileread.txt");
- Scanner sc= new Scanner(file);
- while(sc.hasNextLine())
- System.out.println(sc.nextLine());
- 
-	}}
+      File file=new File("C:\\Selenium training\\Fileread.txt");
+      Scanner sc= new Scanner(file);
+      while(sc.hasNextLine()) {
+      String line= sc.nextLine();
+      int stringsize = line.length();
+	  char c = 'j';
+	  String duplicateChar = "";
+	  for(int i = 0 ; i < stringsize ; i++) {
+		int count = 0;
+		for(int j = 0 ; j < stringsize ; j++) {
+			if(line.charAt(j) == line.charAt(i)) {
+				count++;
+			}
+		}
+       if(count > 1) {
+			if(duplicateChar.contains(String.valueOf(line.charAt(i)))) {
+				
+			}else {
+				System.out.println("maximum character found " + line.charAt(i)  +  " with " + count + " Occurrences" );
+				duplicateChar = duplicateChar + String.valueOf(line.charAt(i)); 
+			 }
+          }
+        }
+      }
+    }
+   }
+
