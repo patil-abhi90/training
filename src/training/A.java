45d15fe36ec48a268e8ba089bed7b0f6cc1c8ab0
Abhijeet <abhijeetpatil2590@gmail.com>
2023-09-04 15:35:23 +0530

    Access modifier

---
+++ src/training/A.java
@@ -0,0 +1,22 @@
+package training;
+
+ public class A{
+ }
+class B{
+ }
+protected class C{
+}
+public class D{
+	
+}
+public class Demo{	
+	
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A a1=new A();
+B a1=new B();
+C a1=new C();
+D a1=new D();
+	}
+}
+
