c39dd7c909dfbf386f8ce28f9da7c42e9aec0f8b
Abhijeet <abhijeetpatil2590@gmail.com>
2023-09-05 19:24:14 +0530

    Java program to print whether it is positive or negative

---
+++ src/training/Positivenegativenumber.java
@@ -0,0 +1,25 @@
+package training;
+
+import java.util.Scanner;
+
+public class Positivenegativenumber {
+	
+  public static void main(String[] args)
+  {
+	Scanner sc=new  Scanner(System.in);
+    System.out.println("insert number :");
+    int i = sc.nextInt();
+    int z=i;
+    if(z>0)
+    {
+    	System.out.println(z+ " is positive");
+    }
+    else if(z<0)
+    {
+    	System.out.println(z+ " is negative");
+    }
+    else {
+    	 
+        System.out.println(z + " is zero.");
+  }
+}}
